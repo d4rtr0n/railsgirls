f8d545b3f25cca93d01b5c0e86d2632da58f25fc
d4rtr0n <daryac.danesh@gmail.com>
2017-03-09 12:43:37 +0100

    adding some railsgirls stuff

---
+++ card_game_darya.rb
@@ -0,0 +1,34 @@
+
+
+
+
+puts "Guess a number between 1 and 14"
+user_number = gets.chomp
+
+if user_number != ""
+  user_number = user_number.to_i # converts string to integer
+end
+
+puts "Choose a suite by choosing a number from 0 to 4; 0 = Hearts, 1 = Diamonds, 2 = Clubs, 3 = Spades"
+user_suite = gets.chomp
+
+if user_suite != ""
+    user_suite = user_suite.to_i
+end
+
+comp_number = rand(1..14)
+
+suites = ['Hearts','Diamonds', 'Clubs', 'Spades']
+comp_suite = rand(suites.length)
+
+#debug this
+guess = "#{user_number} + #{user_suite}"
+card = "#{comp_number} + #{comp_suite}"
+
+puts "You guessed #{guess}, the Computer generated #{card}"
+if guess == card
+  puts "You guessed correctly! You win!"
+else
+  puts "You guessed wrong... You suck!"
+end
+exit
