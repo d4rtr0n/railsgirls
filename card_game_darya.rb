943e5d2ceab58f21675fee13d8c3f79af3f101bd
d4rtr0n <daryac.danesh@gmail.com>
2017-03-13 14:58:09 +0100

    edits

--- card_game_darya.rb
+++ card_game_darya.rb
@@ -3,31 +3,40 @@ user_number = gets.chomp
 
 if user_number.is_a?(Integer)
   user_number = user_number.to_i # converts string to integer
+#why is this running even when i enter an integer?
 else
   puts "Please enter an integer with no decimal places"
   user_number = gets.chomp
 end
-exit
 
-# puts "Choose a suite by choosing a number from 0 to 3; 0 = Hearts, 1 = Diamonds, 2 = Clubs, 3 = Spades"
-# user_suite = gets.chomp
-#
-# if user_suite != ""
-#     user_suite = user_suite.to_i
-# end
-#
-# comp_number = rand(1..14)
-#
-# suites = ['Hearts','Diamonds', 'Clubs', 'Spades']
-# comp_suite = rand(suites.length)
-#
-# guess = "#{user_number} + #{user_suite}"
-# card = "#{comp_number} + #{comp_suite}"
-#
-# puts "You guessed #{guess}, the Computer generated #{card}"
-# if guess == card
-#   puts "You guessed correctly! You win!"
-# else
-#   puts "You guessed wrong... You suck!"
-# end
-# exit
+puts "Please enter a suit: Hearts, Diamonds, Clubs, or Spades"
+user_suit = gets.chomp
+
+# why is my code not running through this loop????
+if user_suit == "hearts" or "Hearts"
+  user_suit = 0
+elsif user_suit == "diamonds" or "Diamonds"
+  user_suit = 1
+elsif  user_suit == "clubs" or "Clubs"
+  user_suit = 2
+elsif user_suit == "spades" or "Spades"
+  user_suit = 3
+else
+  puts "Please enter a valid suit: Hearts, Diamonds, Clubs, or Spades"
+end
+
+comp_number = rand(1..14)
+
+suits = ['hearts','diamonds', 'clubs', 'spades']
+comp_suit = rand(suits.length)
+
+guess = "#{user_number} + #{user_suit}"
+card = "#{comp_number} + #{comp_suit}"
+
+puts "You guessed #{guess}, the Computer generated #{card}"
+if guess == card
+  puts "You guessed correctly! You win!"
+else
+  puts "You guessed wrong... You suck!"
+end
+exit
